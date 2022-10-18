f011e08b4bdc500e2a22bbc4021b39dce22eed34
Auca Maillot <aucacoyan@gmail.com>
2022-10-17 23:31:50 -0300

    add:work on boxes.rs

--- boxes/src/main.rs
+++ boxes/src/main.rs
@@ -1,4 +1,4 @@
-use std::io::stdin;
+use std::{io::stdin, num::IntErrorKind};
 /*
 struct Box {
     contents: String,
@@ -13,25 +13,39 @@ enum Response {
 */
 
 fn main() {
+    println!("Welcome to");
+    println!("Box simulator 1.0");
+
     let mut response = String::new();
 
-    println!("type sth: ");
+    println!("Write the box number: \n(between 0 and 65 535)");
 
     stdin().read_line(&mut response).expect("Failed input");
 
-    println!("you wrote {}", response)
+    let tiny_box = match response
+        .trim_end() // remove the \r\n
+        .parse::<u16>()
+    {
+        Ok(number) => number,
+        Err(error) => match error.kind() {
+            IntErrorKind::PosOverflow => panic!("You can't pick a number greater than 65 535"),
+            IntErrorKind::NegOverflow => panic!("You can't pick a number less than 0"),
+            IntErrorKind::Empty => panic!("I read an empty value"),
+            other_error => {
+                panic!("Something wrong has happened: {:?}", other_error)
+            }
+        },
+    };
+
+    println!("you chose {:?}", tiny_box)
 }
 
 /*
-    println!("Welcome to");
 
     let exit = false;
-    let mut buffer = String::new();
 
     while exit == false {
-        println!();
-        println!("Box simulator 1.0");
-        stdin().read_line(&mut buffer);
+
         let res = match buffer.trim_end() {
             "q" => println!("you pressed q"),
             _ => println!("something has gone wrong"),
