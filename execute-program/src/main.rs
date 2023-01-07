95841bf0d3fe47599c59a96e836cb4da95e3ca82
Auca Maillot <aucacoyan@gmail.com>
2023-01-07 09:19:20 -0300

    :beers: WIP

--- execute-program/src/main.rs
+++ execute-program/src/main.rs
@@ -1,8 +1,8 @@
 //use execute::Execute;
-use std::process::Command;
+use std::{ffi::OsStr, process::Command};
 
 // const ICUE_PATH: &str = "C:\\Program Files\\Corsair\\CORSAIR iCUE 4 Software\\iCUE.exe";
-const EXECUTE: &str = "&";
+const EXECUTE: &str = "Invoke-Item";
 // const PYTHON_TEST: &str = "pipenv";
 
 fn main() {
@@ -10,7 +10,10 @@ fn main() {
     // my_command.arg(r#"--version"#);
     // let path = "'C:\\Program Files\\Corsair\\CORSAIR iCUE 4 Software\\iCUE.exe'";
 
-    my_command.arg("'C:\\Program Files\\Corsair\\CORSAIR iCUE 4 Software\\iCUE.exe'");
+    my_command.arg(r"C:\Program Files\Corsair\CORSAIR iCUE 4 Software\iCUE.exe");
+
+    let passed_args: Vec<&OsStr> = my_command.get_args().collect();
+    println!("{:?}", passed_args);
 
     // execution
     match my_command.output() {
@@ -18,7 +21,7 @@ fn main() {
             println!("output: {}", String::from_utf8_unchecked(o.stdout));
         },
         Err(e) => {
-            println!("There was an error! \n {}", e)
+            println!("There was an error! \n{}", e)
         }
     }
 }
