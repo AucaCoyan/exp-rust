2661eb99bc8565dfe9e3aec6e2b0c41870966315
Auca Maillot <aucacoyan@gmail.com>
2023-04-28 23:50:22 -0300

    :sparkles: structs

--- rust-book/src/main.rs
+++ rust-book/src/main.rs
@@ -1,31 +1,43 @@
-use std::str::FromStr;
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-fn first_word(s: &str) -> usize {
-    let bytes = s.as_bytes();
-
-    for (i, &item) in bytes.iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
     }
 
-    s.len()
-}
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: Option<u64>,
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
+impl Rectangle {
+    fn new_square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
 fn main() {
-    let new_string = String::from("Auca esta programando");
-    let Auca = User {
-        active: true,
-        username: String::new(),
-        email: String::new(),
-        sign_in_count: None,
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
     };
 
-    println!("{:?}", Auca.sign_in_count)
+    let rect2 = Rectangle {
+        width: 40,
+        height: 50,
+    };
+
+    let square = Rectangle::new_square(2);
+
+    println!(
+        "The area of the rectangle is {} square pixels",
+        rect1.area()
+    );
+
+    println!("Can rect1 hold rect 2? {}", rect1.can_hold(&rect2));
 }
