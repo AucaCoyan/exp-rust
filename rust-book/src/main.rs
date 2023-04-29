a362bc0ed7338c4d181c5069ebe2cf3c33920297
Auca Maillot <aucacoyan@gmail.com>
2023-04-29 18:21:19 -0300

    :sparkles: advancements on rust-book

--- rust-book/src/main.rs
+++ rust-book/src/main.rs
@@ -1,52 +1,6 @@
 #![allow(unused)]
-
-use std::str::FromStr;
-
-struct QuitMessage; // unit struct
-struct MoveMessage {
-    x: i32,
-    y: i32,
-}
-#[derive(Debug)]
-struct WriteMessage {
-    string: String,
-    id: u128,
-} // full struct
-struct ChangeColorMessage(i32, i32, i32); // tuple struct
-
-#[derive(Debug)]
-enum MessageType {
-    QuitMsg,
-    MoveMsg,
-    ChatMsg { content: String, id: u128 },
-    ChangeColorMsg,
-}
-
-#[derive(Debug)]
-struct Message {
-    _type: MessageType,
-}
-
-enum Option {
-    None,
-    Some,
-}
-
-impl Message {
-    fn new(mess_type: String) -> Self {
-        Self {
-            _type: MessageType::ChatMsg {
-                content: mess_type,
-                id: 1,
-            },
-        }
-    }
-}
 fn main() {
-    let var = Option::Some;
-
-    let content = String::from("Mi contenido de mensaje");
-    let my_mes = Message::new(String::from("Mi Mensaje"));
-
-    println!("Mi mensaje: {:?}", my_mes)
+    let may_have_value_1: Option<i32> = Some(10);
+    let may_have_value_2: Option<i32> = Some(20);
+    println!("{}", may_have_value_1 + may_have_value_2)
 }
