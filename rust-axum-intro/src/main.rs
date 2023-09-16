f1bcba156f19be837f5e08386dd27ee4a70dad2d
Auca Maillot <auca.maillot@farmacity.com.ar>
2023-09-16 15:50:21 -0300

    :sparkles: `axum` intro

---
+++ rust-axum-intro/src/main.rs
@@ -0,0 +1,24 @@
+#![allow(unused)]
+
+use std::net::SocketAddr;
+
+use axum::response::Html;
+use axum::routing::get;
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let routes_hello = Router::new().route(
+        "/hello",
+        get(|| async { Html("Hello <strong>World!!!</strong>") }),
+    );
+
+    // region:      --- Start Server
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("--> Listenting on {addr}\n");
+    axum::Server::bind(&addr)
+        .serve(routes_hello.into_make_service())
+        .await
+        .unwrap();
+    // end region:  --- Start Server
+}
