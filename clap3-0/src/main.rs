cabdf2a319e20e58290a8bb2e812408c3657e98c
Auca Coyan <aucacoyan@gmail.com>
2024-02-20 11:38:22 -0300

    :fire: clean unused fns and imports

--- clap3-0/src/main.rs
+++ clap3-0/src/main.rs
@@ -1,29 +1,7 @@
-use clap::{arg, App, Parser, Subcommand};
-use log::{debug, error, info, trace, warn, LevelFilter};
+use clap::{Parser, Subcommand};
+use log::{debug, info, trace, warn, LevelFilter};
 use simple_logger::SimpleLogger;
-use std::path::{Path, PathBuf};
-
-fn app() -> App<'static> {
-    App::new("espanso")
-        .arg(arg!([name] "Optional name to operate on"))
-        .arg(
-            arg!(
-                -c --config <FILE> "Sets a custom config file"
-            )
-            // We don't have syntax yet for optional options, so manually calling `required`
-            .required(false)
-            // Support non-UTF8 paths
-            .allow_invalid_utf8(true),
-        )
-        .arg(arg!(
-            -d --debug ... "Turn debugging information on"
-        ))
-        .subcommand(
-            App::new("test")
-                .about("does testing things")
-                .arg(arg!(-l --list "lists test values")),
-        )
-}
+use std::path::PathBuf;
 
 #[derive(Parser)]
 #[clap(author, version, about)]
