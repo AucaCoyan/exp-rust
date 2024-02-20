f203d6f629695b5da90be1eb959c5855d78a32c2
Auca Coyan <aucacoyan@gmail.com>
2024-02-20 10:20:55 -0300

    :test_tube: add failing toml tests

--- clap3-0/tests/cli_tests.rs
+++ clap3-0/tests/cli_tests.rs
@@ -6,6 +6,8 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 fn cli_tests() {
     trycmd::TestCases::new()
         .case("tests/cmd/*.toml")
+        .insert_var("[VERSION]", VERSION)
+        .unwrap()
         .case("tests/expected_result.md")
         // See Issue #314
         .fail("tests/cmd/buggy-case.toml");
