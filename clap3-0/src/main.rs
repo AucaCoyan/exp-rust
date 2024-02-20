77b04ed38005bc3686adc798fdb3a2adc2175bec
Auca Coyan <aucacoyan@gmail.com>
2024-02-20 11:37:29 -0300

    :sparkles: add logging levels

--- clap3-0/src/main.rs
+++ clap3-0/src/main.rs
@@ -1,5 +1,7 @@
-use clap::{arg, App};
-use std::path::Path;
+use clap::{arg, App, Parser, Subcommand};
+use log::{debug, error, info, trace, warn, LevelFilter};
+use simple_logger::SimpleLogger;
+use std::path::{Path, PathBuf};
 
 fn app() -> App<'static> {
     App::new("espanso")
@@ -23,38 +25,91 @@ fn app() -> App<'static> {
         )
 }
 
+#[derive(Parser)]
+#[clap(author, version, about)]
+struct Cli {
+    /// Optional name to operate on
+    name: Option<String>,
+
+    /// Sets a custom config file
+    #[clap(short, long, parse(from_os_str), value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    /// Turn debugging information on
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: usize,
+
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// does testing things
+    Test {
+        /// lists test values
+        #[clap(short, long)]
+        list: bool,
+    },
+}
+
 fn main() {
-    let matches = app().get_matches();
+    let cli_instance = Cli::parse();
 
     // You can check the value provided by positional arguments, or option arguments
-    if let Some(name) = matches.value_of("name") {
+    if let Some(name) = cli_instance.name.as_deref() {
         println!("Value for name: {name}");
     }
 
-    if let Some(raw_config) = matches.value_of_os("config") {
-        let config_path = Path::new(raw_config);
+    if let Some(config_path) = cli_instance.config.as_deref() {
         println!("Value for config: {}", config_path.display());
+    }
+
+    match cli_instance.verbose {
+        0 => SimpleLogger::new()
+            .with_level(LevelFilter::Error)
+            .init()
+            .unwrap(),
+        1 => SimpleLogger::new()
+            .with_level(LevelFilter::Info)
+            .init()
+            .unwrap(),
+        // Trace mode is only available in debug mode for security reasons
+        #[cfg(debug_assertions)]
+        3 => SimpleLogger::new()
+            .with_level(LevelFilter::Trace)
+            .init()
+            .unwrap(),
+        _ => SimpleLogger::new()
+            .with_level(LevelFilter::Debug)
+            .init()
+            .unwrap(),
+    };
+
+    trace!("some trace log");
+    debug!("some debug log");
+    info!("some information log");
+    warn!("some warning log");
+    // error!("some error log");
+
+    if let Some(handler) = &cli_instance.command {
+    } else {
     }
 
     // You can see how many times a particular flag or argument occurred
     // Note, only flags can have multiple occurrences
-    match matches.occurrences_of("debug") {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
 
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level app
-    if let Some(matches) = matches.subcommand_matches("test") {
-        // "$ myapp test" was run
-        if matches.is_present("list") {
-            // "$ myapp test -l" was run
-            println!("Printing testing lists...");
-        } else {
-            println!("Not printing testing lists...");
+    match &cli_instance.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
         }
+        None => {}
     }
 
     // Continued program logic goes here...
