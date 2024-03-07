fb0de5a68e3fd4eacf0d159ba40d338e7f0a664d
Auca Coyan <aucacoyan@gmail.com>
2024-03-07 09:00:21 -0300

    :triangular_flag_on_post: add `all-rspos`

--- all-rspos/src/main.rs
+++ all-rspos/src/main.rs
@@ -1,3 +1,38 @@
+use clap::Parser;
+use log::trace;
+use serde_json::Result;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[clap(version)]
+struct Cli {
+    #[arg(help = "the configuration file")]
+    config_file: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let cli = Cli::parse();
+    trace!("recieved config_file: {:?}", cli.config_file);
+    clone_repos(&cli.config_file);
+}
+
+fn clone_repos(config: &PathBuf) -> Result<()> {
+    if !config.exists() {
+    } else {
+        let config = serde_json::from_str(config)?;
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use clap::CommandFactory;
+
+    #[test]
+    fn clap_cli_contruction() {
+        Cli::command().debug_assert();
+    }
 }
