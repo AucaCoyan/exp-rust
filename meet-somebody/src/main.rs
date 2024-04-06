d2c050509d2078d8a357af11196a538841eb1df6
Auca Coyan <aucacoyan@gmail.com>
2024-04-06 10:51:07 -0300

    :construction: something something not working

--- meet-somebody/src/main.rs
+++ meet-somebody/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use gcloud_sdk::{GoogleApi, GoogleApiClient};
+
+use google_calendar;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    people: Vec<String>
+}
+
+#[tokio::main]
+async fn main() {
+    let api_key = dotenv::var("API_KEY");
+    let client_id = dotenv::var("CLIENT_ID");
+    let client = google_calendar::Client();
+
+    let cli = Args::parse();
+
+    for person in cli.people {
+        println!("You invited {person}!");
+    }
 }
