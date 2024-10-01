5739c5be52eec66fffb03f7553b914e2e69e3cad
Auca Coyan <aucacoyan@gmail.com>
2024-10-01 18:17:33 -0300

    :adhesive_bandage: fix arg parser

--- one-dust/src/main.rs
+++ one-dust/src/main.rs
@@ -3,9 +3,25 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(version, about) ]
 struct Cli {
-    
+    #[command(subcommand)]
+    command: Command,
 }
+#[derive(Subcommand)]
+enum Command {
+    Sync,
+    Monitor,
+    DisplayConfig,
+    DisplaySyncStatus,
+}
+
 fn main() {
-    let args 
+    let args = Cli::parse();
+    match args.command {
+        Command::Sync => println!("output sync"),
+        Command::Monitor => println!("output monitor"),
+        Command::DisplayConfig => println!("output DisplayConfig"),
+        Command::DisplaySyncStatus => println!("output Display sync status"),
+
+    }
     println!("Hello, world!");
 }
