29dd2ca5a77d539c2fbf22c7b3b2ebc3d43b6bf5
Auca Coyan <aucacoyan@gmail.com>
2024-12-22 22:02:34 -0300

    :sparkles: finish a single socket connection (not sharing the db)

--- tokio-mini-redis/src/main.rs
+++ tokio-mini-redis/src/main.rs
@@ -18,14 +18,35 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    let mut db = HashMap::new();
+
     // the connection lets us read/write redis **frames** instead
     // of byte streams. The Connection type is defined by mini-redis.
     let mut connection = Connection::new(socket);
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
 
-        // respond with an error
-        let response = Frame::Error("unimplemented".to_string());
+    while let Some(frame) = connection.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                //value is stores as `Vec<u8>`
+                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    // `Frame::Bulk` expects data to be one of type `Bytes`.
+                    // For now `&Vec<u8>` is converted to `Bytes` using `into()`
+                    Frame::Bulk(value.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("unimplemented {:?}", cmd),
+        };
+
+        // Write the response to the client
         connection.write_frame(&response).await.unwrap();
     }
 }
