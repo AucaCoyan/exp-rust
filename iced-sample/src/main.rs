020f3c75757bc899617a630709a9b353c3f40834
Auca Coyan <aucacoyan@gmail.com>
2025-03-18 17:23:07 -0300

    :construction: work on iced

--- iced-sample/src/main.rs
+++ iced-sample/src/main.rs
@@ -1,3 +1,35 @@
+use iced::widged::text;
+struct Counter {
+    value: i64,
+}
+impl Counter {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
+        }
+    }
+}
+
+enum Message {
+    Increment,
+    Decrement,
+}
+
+fn gui() -> iced::Result {
+    iced::run("Espanso", Counter::update, view)
+}
 fn main() {
+    let mut counter = Counter { value: 0 };
+    counter.update(Message::Decrement);
+    counter.update(Message::Decrement);
+    counter.update(Message::Decrement);
+    counter.update(Message::Decrement);
+    counter.update(Message::Increment);
+
     println!("Hello, world!");
 }
