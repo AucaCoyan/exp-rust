1a78ece349f415bf50f50b8696ecbe7b4d77579f
Auca Maillot <aucacoyan@gmail.com>
2025-07-28 15:12:37 -0300

    :construction: work work work

--- cc_1_2_30_find_tool/src/main.rs
+++ cc_1_2_30_find_tool/src/main.rs
@@ -1,6 +1,9 @@
 use cc;
 fn main() {
-    let tool = cc::windows_registry::find_tool("msvc", "devenv")
-        .expect("unable to locate MSVC compiler, did you install Visual Studio?");
-    println!("Hello, world!");
+    // let tool = cc::windows_registry::find_tool("msvc", "devenv")
+    //     .expect("unable to locate MSVC compiler, did you install Visual Studio?");
+
+    let cc_build = cc::Build::new();
+    let tool_used = cc_build.get_compiler();
+    println!("using {tool_used:#?}");
 }
